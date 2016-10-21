7b03502740d99b4d64873e683d1c10866779c22a
cong_thanh_nguyen <makotovnjp@gmail.com>
2016-10-21 11:24:23 +0900

    complete standard

---
+++ app.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Nguyen Cong Thanh on 2016/10/21.
+ */
+(function () {
+    'use strict'
+
+    angular.module('MsgApp',[])
+        .controller('MsgController',MsgController);
+
+    MsgController.$inject = ['$scope'];
+    
+    function MsgController($scope) {
+        $scope.menuItems = "";
+        $scope.msg = "";
+        
+        $scope.checkMenu = function () {
+            if($scope.menuItems) {
+                var NoMenuItems = $scope.menuItems.split(',').length;
+
+                if (NoMenuItems > 3) {
+                    $scope.msg = "Too much!";
+                } else {
+                    $scope.msg = "Enjoy!";
+                }
+            } else {
+                $scope.msg = "Please enter data first";
+            }
+        };
+    }
+
+})();
